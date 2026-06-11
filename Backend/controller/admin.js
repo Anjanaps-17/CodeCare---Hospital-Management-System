0f5627b33873df5f6aa1853c958b181a8133356b
anjana55413@gmail.com <anjana55413@gmail.com>
2026-06-11 12:39:26 +0530

    Added User, Department and Doctor schemas for admin module

---
+++ Backend/controller/admin.js
@@ -0,0 +1,111 @@
+const { User, Department, Doctor } = require("../models/admin");
+
+
+
+// Create User
+exports.createUser = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+// Get All Users
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+
+    res.status(200).json({
+      success: true,
+      data: users,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+
+
+// Create Department
+exports.createDepartment = async (req, res) => {
+  try {
+    const department = await Department.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: department,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+// Get All Departments
+exports.getDepartments = async (req, res) => {
+  try {
+    const departments = await Department.find();
+
+    res.status(200).json({
+      success: true,
+      data: departments,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+
+
+// Create Doctor
+exports.createDoctor = async (req, res) => {
+  try {
+    const doctor = await Doctor.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: doctor,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+// Get All Doctors
+exports.getDoctors = async (req, res) => {
+  try {
+    const doctors = await Doctor.find()
+      .populate("userId")
+      .populate("department");
+
+    res.status(200).json({
+      success: true,
+      data: doctors,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
